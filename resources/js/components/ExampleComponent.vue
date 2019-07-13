ca4788b9081be85a8fbd7a9630212a37bb67256d
talivan5 <talivan51987@gmail.com>
2019-07-13 02:52:24 -0400

    ultimos cambios a subir con el mapa

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -3,10 +3,25 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">ivan flores</div>
+                    <div class="card-header">Busqueda de Restaurantes</div>
 
                     <div class="card-body">
-                        I'm an example component.
+
+                            <gmap-map
+                            :center="{lat:-17.964434, lng:-67.103808}"
+                            :zoom="16"
+                            map-type-id="terrain"
+                            style="width: 700px; height: 300px"
+                            ref="googleMap"
+                            >
+
+                            </gmap-map>
+                    </div>
+                    <div class="card-footer">
+                       <a href="" class="btn btn-primary"
+                            @click="getLocation()"
+                       >
+                       Sacar Localización</a>
                     </div>
                 </div>
             </div>
@@ -15,9 +30,30 @@
 </template>
 
 <script>
+import Vue from 'vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyBCDlud_eL49CBc7jYoowFOdlnf-dpi_tw'
+
+  },
+
+
+})
     export default {
         mounted() {
             console.log('Component mounted.')
+        },
+        methods:{
+            getLocation(){
+                console.log(this.$refs.googleMap.$mapObject.getCenter());
+            }
         }
     }
 </script>
+
+<style lang="less">
+
+</style>
+
+
