11e3c804e215e934c8b8276297c05974d36890e7
talivan5 <talivan51987@gmail.com>
2019-07-15 23:34:33 -0400

    cambios de windows a linux

--- public/js/app.js
+++ public/js/app.js
@@ -79645,8 +79645,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\apigoogle\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\apigoogle\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/html/apigoogle/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/html/apigoogle/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
